2d7373cf5935cff5cda9fe8ba196fb08f7ce7f93
Nikzin00 <126522986+Nikzin00@users.noreply.github.com>
2024-08-05 22:52:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Nikolas Matheus";
+const idade = 17;
+const altura = 1.73;
+const peso = 65;
+const cidade = "São José dos Pinhais"
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(peso);
+console.log(cidade);
